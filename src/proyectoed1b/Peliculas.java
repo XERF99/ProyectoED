a3fa2e2dbd5653cbd98ceb5c3d68b37f9f5763f9
FRANCISCO <FRANCISCO@LAPTOP-U5MEVHDQ>
2021-07-14 21:10:36 -0500

    Se creó la clase Películas en el proyecto

---
+++ src/proyectoed1b/Peliculas.java
@@ -0,0 +1,73 @@
+
+package proyectoed1b;
+
+/**
+ *
+ * @author barragán - rodríguez - guevara
+ */
+public class Peliculas {
+
+    private int ip;
+    private String nombre;
+    private int año;
+    private int calificacion;
+    private String genero;
+    private String estado;
+
+    public Peliculas(int ip, String nombre, int año, int calificacion, String genero, String estado) {
+        this.ip = ip;
+        this.nombre = nombre;
+        this.año = año;
+        this.calificacion = calificacion;
+        this.genero = genero;
+        this.estado = estado;
+    }
+
+    public int getIp() {
+        return ip;
+    }
+
+    public void setIp(int ip) {
+        this.ip = ip;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
